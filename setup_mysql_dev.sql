ce41c47b4488964310e16d73e8f2c8ab74ac59a6
davidbirenzi <d.birenzi@alustudent.com>
2025-01-21 14:13:06 +0200

    setup mysql test

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,14 +1,9 @@
--- Create the database hbnb_dev_db if it does not exist
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
--- Create the user hbnb_dev with the specified password if it does not exist
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
--- Grant all privileges on hbnb_dev_db to hbnb_dev
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
--- Grant SELECT privilege on the performance_schema database to hbnb_dev
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 
--- Apply the changes
 FLUSH PRIVILEGES;
