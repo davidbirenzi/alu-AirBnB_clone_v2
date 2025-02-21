97ea9deb4540c13c808b6a661ce8c725c1ac2525
Dushimepaulette1 <p.dushime1@alustudent.com>
2025-02-21 19:32:58 +0000

    web_flaskgit

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -8,3 +8,12 @@ def hello_hbnb():
     """Returns a simple greeting message."""
     return "Hello HBNB!"
 
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Returns 'HBNB' when visiting /hbnb."""
+    return "HBNB" 
+
+if __name__ == "__main__":
+    # Running the app on all available IPs (0.0.0.0) and port 5000
+    app.run(host='0.0.0.0', port=5000)  
+
