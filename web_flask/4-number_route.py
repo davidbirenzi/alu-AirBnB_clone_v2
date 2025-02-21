f0df6c8f852823c9cb24c00b4fc663c961c870d0
Dushimepaulette1 <p.dushime1@alustudent.com>
2025-02-21 19:53:01 +0000

    web flask

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+# Creating the Flask app instance
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Returns a simple greeting message."""
+    return "Hello HBNB!"
+
+
