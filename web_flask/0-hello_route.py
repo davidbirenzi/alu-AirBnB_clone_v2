08f225ecf27bde4981506d2c1a6a043af6d70a57
davidbirenzi <d.birenzi@alustudent.com>
2025-02-21 14:44:49 +0200

    Added a shebang

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,7 +8,6 @@ A simple Flask web application.
 from flask import Flask
 
 app = Flask(__name__)
-
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """Returns a simple greeting message."""
