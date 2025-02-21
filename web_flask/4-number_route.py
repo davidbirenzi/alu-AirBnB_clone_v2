023456b99e9c47b111fcad16c8375975a45c8e73
Dushimepaulette1 <p.dushime1@alustudent.com>
2025-02-21 20:07:49 +0000

    web flask

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -29,3 +29,12 @@ def python_text(text):
     If no text is provided, defaults to 'is cool'.
     """
     return "Python " + text.replace("_", " ")
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_route(n):
+    """Returns 'n is a number' only if n is an integer."""
+    return f"{n} is a number"
+
+if __name__ == "__main__":
+    # Running the app on all available IPs (0.0.0.0) and port 5000
+    app.run(host='0.0.0.0', port=5000)
