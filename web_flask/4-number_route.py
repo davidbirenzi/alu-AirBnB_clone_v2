177c8a1135f0db9845eaee54f17f1f3d6fdd9c8e
Dushimepaulette1 <p.dushime1@alustudent.com>
2025-02-21 20:04:00 +0000

    web flask

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python3
+"""
+Flask web application that handles different routes with dynamic parameters
+"""
 from flask import Flask
 
 # Creating the Flask app instance
@@ -7,5 +11,10 @@ app = Flask(__name__)
 def hello_hbnb():
     """Returns a simple greeting message."""
     return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Returns 'HBNB' when visiting /hbnb."""
+    return "HBNB"
 
 
