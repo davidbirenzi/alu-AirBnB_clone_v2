5a69f80815c27710c9bf3696e0494d6b1e2c7c49
davidbirenzi <d.birenzi@alustudent.com>
2025-02-22 15:37:30 +0200

    documented

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -3,12 +3,9 @@
 A simple Flask web application.
 """
 
-
 from flask import Flask
 
-# Creating the Flask app instance
 app = Flask(__name__)
-
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """Returns a simple greeting message."""
@@ -22,7 +19,7 @@ def hbnb():
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
     """Returns 'C' followed by the given text, with underscores replaced by spaces."""
-    return "C " + text.replace("_", " ")  # Dynamic route magic!
-f __name__ == "__main__":
-    # Running the app on all available IPs (0.0.0.0) and port 5000
+    return "C " + text.replace("_", " ")
+if __name__ == "__main__":
+    """Running the app on all available IPs (0.0.0.0) and port 5000"""
     app.run(host='0.0.0.0', port=5000)
