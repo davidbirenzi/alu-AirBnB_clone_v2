be4b2b28a9a3005855688922569c770863242300
davidbirenzi <d.birenzi@alustudent.com>
2025-02-22 23:24:26 +0200

    Number 1

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Start a Flask web application"""
+"""Flask web application"""
 from flask import Flask
 
 app = Flask(__name__)
--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Starts the web app"""
+"""web app"""
 from flask import Flask
 from urllib.parse import unquote
 
@@ -8,13 +8,13 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """Displays the Hello HBNB"""
+    """Displays Hello HBNB"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def display_hbnb():
-    """Returns the HBNB"""
+    """Returns HBNB"""
     return 'HBNB'
 
 
